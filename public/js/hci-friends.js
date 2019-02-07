8379e36ac459a909f1d4690007633a67e4669c25
coraxyc <ycxing99@gmail.com>
2019-02-07 02:40:22 -0800

    Completed lab5

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -14,10 +14,10 @@ function initializePage() {
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
+
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
-	} 
+	}
 	else if (name == "Ivan Sutherland") {
 		return "Vandal Heist Run";
 	}
@@ -44,3 +44,8 @@ function anagrammedName(name) {
 		return name;
 	}
 }
+
+$('.friend a').click(function(e) {
+	e.preventDefault();
+	$(this).text(anagrammedName($(this).text()));
+});
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,13 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
+exports.addFriend = function(request, response) { 
+  let name = request.query.name;
+	let description = request.query.description;
+	let obj = {
+		name : name,
+		description : description,
+		imageURL : 'http://lorempixel.com/500/500/people'
+	};
+
+	data.friends.push(obj);
  }
